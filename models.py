260f87af5f8a00fb6a91ad9ac9d655ed4fc28d8d
Tsaloumas Ioannis <itsaloumas@athtech.gr>
2025-04-06 02:13:43 +0300

    Setting again the timezone to UTC from EET

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer, String, String, Float, DateTime
+# models.py
+from sqlalchemy import Column, Integer, String, Float, DateTime
 from datetime import datetime
 from .database import Base
-from zoneinfo import ZoneInfo
 
 class Expense(Base):
-    __tablename__="expenses"
+    __tablename__ = "expenses"
 
     id = Column(Integer, primary_key=True, index=True)
-    description = Column(String, nullable =False)
+    description = Column(String, nullable=False)
     amount = Column(Float, nullable=False)
-    category = Column(String, nullable = False)
-    date = Column(DateTime, default = datetime.now(ZoneInfo("Europe/Athens")))
+    category = Column(String, nullable=False)
+    date = Column(DateTime, default=datetime.utcnow)
+
